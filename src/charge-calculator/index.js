c4c6b7945e6641300d2ba4b3044bce442c6eac15
Kaio Silveira <silveira.kaio@icloud.com>
2024-10-26 20:20:55 +0100

    remove unused `baseCharge` getter from `ChargeCalculator`

--- src/charge-calculator/index.js
+++ src/charge-calculator/index.js
@@ -5,10 +5,6 @@ export class ChargeCalculator {
     this._provider = provider;
   }
 
-  get baseCharge() {
-    return this._customer.baseRate * this._usage;
-  }
-
   get charge() {
     const baseCharge = this._customer.baseRate * this._usage;
     return baseCharge + this._provider.connectionCharge;
