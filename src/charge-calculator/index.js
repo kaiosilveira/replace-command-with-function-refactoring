7a8bba2b618f9876a90b2aba11097381fa45ae37
Kaio Silveira <silveira.kaio@icloud.com>
2024-10-26 20:28:50 +0100

    remove `ChargeCalculator`

--- src/charge-calculator/index.js
+++
@@ -1,8 +0,0 @@
-export class ChargeCalculator {
-  constructor() {}
-
-  charge(customer, usage, provider) {
-    const baseCharge = customer.baseRate * usage;
-    return baseCharge + provider.connectionCharge;
-  }
-}
