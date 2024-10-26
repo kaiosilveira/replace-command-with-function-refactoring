68ad9d9be5a6195e663f9cdae09614775f54cac8
Kaio Silveira <silveira.kaio@icloud.com>
2024-10-26 20:18:35 +0100

    extract `ChargeCalculator` usage into a `charge` function at `calculateMonthCharge`

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import { ChargeCalculator } from './charge-calculator';
 
 export function calculateMonthCharge(customer, usage, provider) {
-  const monthCharge = new ChargeCalculator(customer, usage, provider).charge;
+  const monthCharge = charge(customer, usage, provider);
   return monthCharge;
+}
+
+function charge(customer, usage, provider) {
+  return new ChargeCalculator(customer, usage, provider).charge;
 }
